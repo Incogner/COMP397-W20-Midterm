4e9f00f3d69b3fd38f11fbd32e7a91cb8d1d8f7c
Nanoteknology <36832557+Nanoteknology@users.noreply.github.com>
2020-02-22 10:45:25 -0500

    Create Play Scene and game functionality

--- Scripts/scenes/Play.js
+++ Scripts/scenes/Play.js
@@ -16,7 +16,6 @@ var scenes;
 (function (scenes) {
     var Play = /** @class */ (function (_super) {
         __extends(Play, _super);
-        // PRIVATE INSTANCE MEMBERS
         // PUBLIC PROPERTIES
         // CONSTRUCTOR
         function Play() {
@@ -25,14 +24,59 @@ var scenes;
             return _this;
         }
         // PRIVATE METHODS
+        /**
+         * This Method will generate a random number (1-6)
+         *
+         * @protected
+         * @returns {number}
+         * @memberof Play
+         */
+        Play.prototype._generateRandomNumber = function () {
+            return Math.floor(Math.random() * 6);
+        };
         // PUBLIC METHODS
         //initialize and instatiate
         Play.prototype.Start = function () {
+            // Buttons
+            this._rollButton = new objects.Button(config.Game.ASSETS.getResult("rollButton"), 320, 430, true);
+            this._dices = new Array();
+            this._dices.push(config.Game.ASSETS.getResult('dice01'));
+            this._dices.push(config.Game.ASSETS.getResult('dice02'));
+            this._dices.push(config.Game.ASSETS.getResult('dice03'));
+            this._dices.push(config.Game.ASSETS.getResult('dice04'));
+            this._dices.push(config.Game.ASSETS.getResult('dice05'));
+            this._dices.push(config.Game.ASSETS.getResult('dice06'));
+            // Dices
+            this._dice1 = new objects.Dice(this._dices[0], 150, 170);
+            this._dice2 = new objects.Dice(this._dices[0], 480, 170);
+            // Labels
+            this._dice1Txt = new objects.Label("1", "40px", "Arial", "black", 150, 310, true);
+            this._dice2Txt = new objects.Label("1", "40px", "Arial", "black", 480, 310, true);
             this.Main();
         };
         Play.prototype.Update = function () {
         };
         Play.prototype.Main = function () {
+            var that = this;
+            this.addChild(this._dice1);
+            this.addChild(this._dice2);
+            this.addChild(this._dice1Txt);
+            this.addChild(this._dice2Txt);
+            this.addChild(this._rollButton);
+            this._rollButton.on("click", function () {
+                var rand01 = that._generateRandomNumber();
+                var rand02 = that._generateRandomNumber();
+                that._dice1 = new objects.Dice(that._dices[rand01], 150, 170);
+                that._dice2 = new objects.Dice(that._dices[rand02], 480, 170);
+                that._dice1Txt.text = (rand01 + 1).toFixed().toString();
+                that._dice2Txt.text = (rand02 + 1).toFixed().toString();
+                that.removeAllChildren();
+                that.addChild(that._rollButton);
+                that.addChild(that._dice1Txt);
+                that.addChild(that._dice2Txt);
+                that.addChild(that._dice1);
+                that.addChild(that._dice2);
+            });
         };
         return Play;
     }(objects.Scene));
--- Scripts/scenes/Play.ts
+++ Scripts/scenes/Play.ts
@@ -3,6 +3,12 @@ module scenes
     export class Play extends objects.Scene
     {
         // PRIVATE INSTANCE MEMBERS
+        private _rollButton:objects.Button;
+        private _dice1:objects.GameObject;
+        private _dice2:objects.GameObject;
+        private _dices:object[];
+        private _dice1Txt:objects.Label;
+        private _dice2Txt:objects.Label;
  
 
         // PUBLIC PROPERTIES
@@ -16,15 +22,43 @@ module scenes
         }
 
         // PRIVATE METHODS
+        /**
+         * This Method will generate a random number (1-6)
+         *
+         * @protected
+         * @returns {number}
+         * @memberof Play
+         */
+        protected _generateRandomNumber():number{
+            return Math.floor(Math.random() * 6);
+        }
 
         // PUBLIC METHODS
 
         //initialize and instatiate
         public Start(): void 
         {
+            // Buttons
+            this._rollButton = new objects.Button(config.Game.ASSETS.getResult("rollButton"), 320, 430, true);
+
+            this._dices = new Array();
+            this._dices.push(config.Game.ASSETS.getResult('dice01'));
+            this._dices.push(config.Game.ASSETS.getResult('dice02'));
+            this._dices.push(config.Game.ASSETS.getResult('dice03'));
+            this._dices.push(config.Game.ASSETS.getResult('dice04'));
+            this._dices.push(config.Game.ASSETS.getResult('dice05'));
+            this._dices.push(config.Game.ASSETS.getResult('dice06'));
             
 
-            
+            // Dices
+            this._dice1 = new objects.Dice(this._dices[0], 150, 170);
+            this._dice2 = new objects.Dice(this._dices[0], 480, 170);
+
+            // Labels
+            this._dice1Txt = new objects.Label("1", "40px", "Arial", "black", 150, 310, true);
+            this._dice2Txt = new objects.Label("1", "40px", "Arial", "black", 480, 310, true);
+
+
              this.Main();
         }        
         
@@ -36,8 +70,33 @@ module scenes
         public Main(): void 
         {
 
+            let that = this;
+            this.addChild(this._dice1);
+            this.addChild(this._dice2);
+            this.addChild(this._dice1Txt);
+            this.addChild(this._dice2Txt);
+            this.addChild(this._rollButton);
+            this._rollButton.on("click", ()=>{
+                
+                let rand01 = that._generateRandomNumber();
+                let rand02 = that._generateRandomNumber();
+
+                that._dice1 = new objects.Dice(that._dices[rand01], 150, 170);
+                that._dice2 = new objects.Dice(that._dices[rand02], 480, 170);
+                that._dice1Txt.text = (rand01 + 1).toFixed().toString();
+                that._dice2Txt.text = (rand02 + 1).toFixed().toString();
+
+                that.removeAllChildren();
+                that.addChild(that._rollButton);
+                that.addChild(that._dice1Txt);
+                that.addChild(that._dice2Txt);
+                that.addChild(that._dice1);
+                that.addChild(that._dice2);
+            });
 
         }
+
+
 
         
     }
